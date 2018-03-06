60644b08d9f17a1b4bdc6ef2b2886d0bb5233aff
Mario García <36269782+mgarciagar@users.noreply.github.com>
2018-03-06 19:20:44 +0100

    Añadido saludo2

--- src/nuevoEjercicio/Hola.java
+++ src/nuevoEjercicio/Hola.java
@@ -3,7 +3,7 @@ package nuevoEjercicio;
 public class Hola {
 
 	public static void main(String[] args) {
-		System.out.println("Hola, ¿Qué tal?");
+		System.out.println("Hola, ¿Qué tal estás?");
 	}
 
 }
